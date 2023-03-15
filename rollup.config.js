1bbba1be71959cc668dfcf74d72385c63f846eac
kirill svetlov <kereal@gmail.com>
2023-03-16 00:28:34 +0300

    обновил почти все

--- rollup.config.js
+++ rollup.config.js
@@ -1,108 +1,122 @@
-import resolve from 'rollup-plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
-import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
-import sveltePreprocess from 'svelte-preprocess';
+import resolve from "rollup-plugin-node-resolve"
+import replace from "@rollup/plugin-replace"
+import commonjs from "rollup-plugin-commonjs"
+import svelte from "rollup-plugin-svelte"
+import babel from "@rollup/plugin-babel"
+import json from "@rollup/plugin-json"
+import terser from "@rollup/plugin-terser"
+import config from "sapper/config/rollup.js"
+import pkg from "./package.json"
+import sveltePreprocess from "svelte-preprocess"
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const mode = process.env.NODE_ENV
+const dev = mode === "development"
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
-const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+const onwarn = (warning, onwarn) =>
+  (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning)
+const dedupe = (importee) => importee === "svelte" || importee.startsWith("svelte/")
 
 export default {
-	client: {
-		input: config.client.input(),
-		output: config.client.output(),
-		plugins: [
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true,
-				preprocess: sveltePreprocess({postcss: true}),
-			}),
-			resolve({
-				browser: true,
-				dedupe
-			}),
-			commonjs(),
-			json(),
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+        preventAssignment: true
+      }),
+      svelte({
+        emitCss: true,
+        preprocess: sveltePreprocess({ postcss: true }),
+        compilerOptions: {
+          dev,
+          hydratable: true
+        }
+      }),
+      resolve({
+        browser: true,
+        dedupe
+      }),
+      commonjs(),
+      json(),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				runtimeHelpers: true,
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+      legacy &&
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          runtimeHelpers: true,
+          exclude: ["node_modules/@babel/**"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: "> 0.25%, not dead"
+              }
+            ]
+          ],
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true
+              }
+            ]
+          ]
+        }),
 
-			!dev && terser({
-				module: true
-			})
-		],
+      !dev && terser()
+    ],
 
-		onwarn,
-	},
+    onwarn
+  },
 
-	server: {
-		input: config.server.input(),
-		output: config.server.output(),
-		plugins: [
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				generate: 'ssr',
-				emitCss: true,
-				preprocess: sveltePreprocess({postcss: true}),
-				dev
-			}),
-			resolve({
-				dedupe
-			}),
-			commonjs(),
-			json()
-		],
-		external: Object.keys(pkg.dependencies).concat(
-			require('module').builtinModules || Object.keys(process.binding('natives'))
-		),
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      replace({
+        "process.browser": false,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+        preventAssignment: true
+      }),
+      svelte({
+        emitCss: true,
+        preprocess: sveltePreprocess({ postcss: true }),
+        compilerOptions: {
+          generate: "ssr",
+          dev
+        }
+      }),
+      resolve({
+        dedupe
+      }),
+      commonjs(),
+      json()
+    ],
+    external: Object.keys(pkg.dependencies).concat(
+      require("module").builtinModules || Object.keys(process.binding("natives"))
+    ),
 
-		onwarn,
-	},
+    onwarn
+  },
 
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+        preventAssignment: true
+      }),
+      commonjs(),
+      !dev && terser()
+    ],
 
-		onwarn,
-	}
-};
+    onwarn
+  }
+}
--- src/routes/auth/getcode.json.js
+++ src/routes/auth/getcode.json.js
@@ -1,10 +1,10 @@
-import nanoid from "nanoid";
-import fs from "fs";
+import { nanoid } from "nanoid"
+import fs from "fs"
 
 export function post(req, res) {
-  const emailCode = nanoid();
-  fs.writeFileSync("code.txt");
-  res.setHeader("Content-Type", "application/json");
+  const emailCode = nanoid()
+  fs.writeFileSync("code.txt")
+  res.setHeader("Content-Type", "application/json")
 
-  res.end(JSON.stringify({ mail: req.body.email }));
+  res.end(JSON.stringify({ mail: req.body.email }))
 }
